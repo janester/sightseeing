2f975ee01160b8736066ea42ad642ead479a5b52
Jane Sternbach <janesternbach@gmail.com>
2013-04-10 22:31:39 -0400

    locations

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sightseeing::Application.config.secret_token = '5f94988dd9db60dca82c116ce49094dd9297e01fd79e5beaa36dfb28ae9430011908f1041dbb145fbef8ff8e1ad179541bb90bda99fd284cfcec34724139f0fd'
